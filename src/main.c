95c54697bb4119294911daab3fe4046e832a566f
Luca Marcelli <luca@marcelli.ch>
2024-07-28 15:30:34 +0200

    update files

--- src/main.c
+++ src/main.c
@@ -3,7 +3,8 @@
 
 #include "common.h"
 
+// [03000000] = 6 / 3 = 2
 void __attribute__((optimize("O0"))) main(void) {
-    asm("mov r0, #6\nmov r1, #3\nswi #0x60000");
+    asm("mov r0, #6\nmov r1, #3\nmov r11, #0x3000000\nswi #0x60000");
     while(true);
 }
